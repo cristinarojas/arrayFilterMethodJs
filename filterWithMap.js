2f7f85f979adbdb7742a7f24bc605d9c4018308a
Cristina Rojas Zamora <criss@Cristinas-MacBook-Pro-2.local>
2017-07-13 12:55:03 -0700

    Using map and sort over filter result array

---
+++ filterWithMap.js
@@ -0,0 +1,19 @@
+// Filtering the array instead of manually looping through the arrays
+// Returns the elements (in one array) of an array that meet the condition specified in a callback function.
+
+var fruits = [
+	{ name: "apple", color: "red"},
+	{ name: "mango", color: "yellow"},
+	{ name: "mango", color: "green"}
+];
+
+var myFruit = fruits.filter(function(item) {
+	return (item.name === 'mango');
+}).map(function(item) {
+	return item.color;
+});
+
+console.log(myFruit);
+
+// Output: [ "yellow", "green"
+// ]
